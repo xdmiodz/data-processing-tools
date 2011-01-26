6a1bb9fcfaf75c700f535d051c3773e6d95f815d
Dmitry Odzerikho <oddi@oddi-desktop.(none)>
2011-01-26 11:44:33 +0300

    add two more tools. descriptiom will be available later

--- ne.m
+++ ne.m
@@ -1,4 +1,5 @@
 function [ne] = ne(fres, f0)
     m = 9.109382e-28;
     e = -4.803204e-10;
-    ne = (pi*m/(e^2))*(fres - f0).^2;
+    fpe2 = (fres.^2 - f0^2);
+    ne = fpe2/((5.64e4/(2*pi))^2);
--- normne.m
+++ normne.m
@@ -1,4 +1,6 @@
 function [nenorm] = normne(ne)
     for i=1:1:size(ne,2)
         nenorm(:,i) = ne(:,i)/max(ne(:,i));
+        a = sum(nenorm(1:1:50,i));
+        nenorm(:,i) = nenorm(:,i)/a;
     end
